70a18bbfc02c34adb87399d070d34c06eecf3df1
EstherWI <esthersss09@gmail.com>
2021-03-11 17:23:20 -0300

    ajuste no tratamento de comentarios

--- src/controller/AnalisadorLexico.java
+++ src/controller/AnalisadorLexico.java
@@ -62,197 +62,198 @@ public class AnalisadorLexico {
         delimitadores.add("{");
         delimitadores.add("}");
         delimitadores.add(".");
-        
+
     }
 
     public String analise(Iterator linhas) {
         this.linhas = linhas;
         int line = 0;
-        int lineComment=0;
+        int lineComment = 0;
+        inComment= false;
         while (linhas.hasNext()) {
             line++;
             linha = (String) linhas.next();
-            if(inComment){
-               estado = 12;
-            }
-            else{
+            if (inComment) {
+                estado = 12;
+            } else {
                 estado = 0;
                 lexema = "";
             }
-            
+
             int size = linha.length();
             char caractere = ' ';
-            
+
             boolean caractereExcedente = false;
-            for (int i = 0; i <=size||caractereExcedente; i++) {
+            for (int i = 0; i <= size || caractereExcedente; i++) {
                 //System.out.print("i: "+i+"; ");
-                if(!caractereExcedente){
-                    if(i!=size)
+                if (!caractereExcedente) {
+                    if (i != size) {
                         caractere = linha.charAt(i);
-                }else
+                    }
+                } else {
                     i--;
-                
+                }
+
                 switch (estado) {
                     case 0:
-                        caractereExcedente=false;
+                        caractereExcedente = false;
                         if (ReconhecedorCaracteres.isSpace(caractere)) {
                             estado = 0;
                             caractereExcedente = false;
-                        }
-                        else if (ReconhecedorCaracteres.isChar(caractere)) {
+                        } else if (ReconhecedorCaracteres.isChar(caractere)) {
                             lexema += caractere;
                             estado = 1;
-                        }else if(ReconhecedorCaracteres.isDigit(caractere)){
+                        } else if (ReconhecedorCaracteres.isDigit(caractere)) {
                             lexema += caractere;
                             estado = 3;
-                        }else if(delimitadores.contains(""+caractere)&&size!=i){
-                            lexema+=caractere;
+                        } else if (delimitadores.contains("" + caractere) && size != i) {
+                            lexema += caractere;
                             this.addToken("DEL", lexema, line);
-                        }else if (caractere == '|'){
+                        } else if (caractere == '|') {
                             estado = 7;
-                            lexema+=caractere;
-                        }else if (caractere == '&'){
+                            lexema += caractere;
+                        } else if (caractere == '&') {
                             estado = 8;
-                            lexema+=caractere;
-                        }else if(caractere == '!'){
+                            lexema += caractere;
+                        } else if (caractere == '!') {
                             estado = 9;
-                            lexema+=caractere;
-                        }else if(caractere == '<' || caractere=='>'){
+                            lexema += caractere;
+                        } else if (caractere == '<' || caractere == '>') {
                             estado = 10;
-                            lexema+=caractere;
-                        }else if(caractere == '='){
+                            lexema += caractere;
+                        } else if (caractere == '=') {
                             estado = 10;
-                            lexema+=caractere;
-                        }else if(caractere == '/'){
+                            lexema += caractere;
+                        } else if (caractere == '/') {
                             estado = 11;
-                        }else{
+                        } else {
                             lexema = "";
                         }
                         break;
                     case 1:
-                        if ((ReconhecedorCaracteres.isChar(caractere) || ReconhecedorCaracteres.isDigit(caractere) || caractere == 95)&&i!=size){
+                        if ((ReconhecedorCaracteres.isChar(caractere) || ReconhecedorCaracteres.isDigit(caractere) || caractere == 95) && i != size) {
                             lexema += caractere;
                             //System.out.println("AQUI GARAI: "+caractere+" i: "+i+" size: "+size+" Excedente: "+caractereExcedente);
-                        }else{
+                        } else {
                             estado = 2;
                             caractereExcedente = true;
                         }
                         break;
                     case 2:
                         estado = 0;
-                        if(palavrasReservadas.contains(lexema))
-                            this.addToken("PRE",lexema, line);
-                        else
-                            this.addToken("IDE",lexema, line);
+                        if (palavrasReservadas.contains(lexema)) {
+                            this.addToken("PRE", lexema, line);
+                        } else {
+                            this.addToken("IDE", lexema, line);
+                        }
                         //caractereExcedente = false;
                         break;
                     case 3:
-                        if (ReconhecedorCaracteres.isChar(caractere)){
-                            lexema+=caractere;
+                        if (ReconhecedorCaracteres.isChar(caractere)) {
+                            lexema += caractere;
                             estado = 6;
-                        }else if (ReconhecedorCaracteres.isDigit(caractere)) {
+                        } else if (ReconhecedorCaracteres.isDigit(caractere)) {
                             lexema += caractere;
                         } else if (ReconhecedorCaracteres.isDot(caractere)) {
                             lexema += caractere;
                             estado = 4;
-                        }else{
+                        } else {
                             estado = 0;
-                            caractereExcedente=true;
-                            this.addToken("NRO",lexema, line);
+                            caractereExcedente = true;
+                            this.addToken("NRO", lexema, line);
                         }
                         break;
                     case 4:
-                        if(ReconhecedorCaracteres.isDigit(caractere)){
-                            lexema+=caractere;
+                        if (ReconhecedorCaracteres.isDigit(caractere)) {
+                            lexema += caractere;
                             estado = 5;
-                        }else{
-                            caractereExcedente=true;
-                            this.addToken("NMF",lexema, line);
+                        } else {
+                            caractereExcedente = true;
+                            this.addToken("NMF", lexema, line);
                             estado = 0;
                         }
                         break;
                     case 5:
-                        if(ReconhecedorCaracteres.isDigit(caractere)){
-                            lexema+=caractere;
-                        }else{
-                            caractereExcedente=true;
-                            this.addToken("NRO",lexema, line);
-                            estado=0;
+                        if (ReconhecedorCaracteres.isDigit(caractere)) {
+                            lexema += caractere;
+                        } else {
+                            caractereExcedente = true;
+                            this.addToken("NRO", lexema, line);
+                            estado = 0;
                         }
                         break;
                     case 6:
-                        if(ReconhecedorCaracteres.isChar(caractere)){
-                            lexema+=caractere;
-                        }else{
-                            this.addToken("NMF",lexema, line);
+                        if (ReconhecedorCaracteres.isChar(caractere)) {
+                            lexema += caractere;
+                        } else {
+                            this.addToken("NMF", lexema, line);
                             estado = 0;
                         }
                         break;
                     case 7:
-                        if(caractere=='|'){
-                            lexema+=caractere;
+                        if (caractere == '|') {
+                            lexema += caractere;
                             this.addToken("LOG", lexema, line);
-                            estado=0;
-                        }else{
-                            lexema+=caractere;
+                            estado = 0;
+                        } else {
+                            lexema += caractere;
                             this.addToken("OpMF", lexema, line);
-                            estado=0;
+                            estado = 0;
                         }
                         break;
                     case 8:
-                        if(caractere=='&'){
-                            lexema+=caractere;
+                        if (caractere == '&') {
+                            lexema += caractere;
                             this.addToken("LOG", lexema, line);
-                            estado=0;
-                        }else{
-                            lexema+=caractere;
+                            estado = 0;
+                        } else {
+                            lexema += caractere;
                             this.addToken("OpMF", lexema, line);
-                            estado=0;
+                            estado = 0;
                         }
                         break;
                     case 9:
-                        if(caractere=='='){
-                            lexema+=caractere;
+                        if (caractere == '=') {
+                            lexema += caractere;
                             this.addToken("REL", lexema, line);
-                            estado=0;
-                        }else{
-                            caractereExcedente=true;
+                            estado = 0;
+                        } else {
+                            caractereExcedente = true;
                             this.addToken("LOG", lexema, line);
-                            estado=0;
+                            estado = 0;
                         }
                         break;
                     case 10:
-                        if(caractere=='='){
-                            lexema+=caractere;
+                        if (caractere == '=') {
+                            lexema += caractere;
                             this.addToken("REL", lexema, line);
-                            estado=0;
-                        }else{
-                            caractereExcedente=true;
+                            estado = 0;
+                        } else {
+                            caractereExcedente = true;
                             this.addToken("REL", lexema, line);
-                            estado=0;
+                            estado = 0;
                         }
                         break;
                     case 11:
-                        if(caractere=='/'){
-                            i=size+1;//saindo do laço que percorre a linha
-                        }else if(caractere=='*'){
-                            estado=12;
-                            inComment=true;
+                        if (caractere == '/') {
+                            i = size + 1;//saindo do laço que percorre a linha
+                        } else if (caractere == '*') {
+                            estado = 12;
+                            inComment = true;
                             lineComment = line;
                         }
                         break;
                     case 12:
-                        if(caractere=='*'){
+                        if (caractere == '*') {
                             estado = 13;
-                        }
-                        else{
-                            estado=12;
+                        } else {
+                            estado = 12;
                         }
                         break;
                     case 13:
-                        if(caractere=='/'){
-                            estado=0;
-                            inComment=false;
+                        if (caractere == '/') {
+                            estado = 0;
+                            inComment = false;
                         }
                         break;
                     default:
@@ -262,7 +263,7 @@ public class AnalisadorLexico {
             }
         }
         //se quando a leitura terminar o comentario ainda estiver aberto
-        if(inComment){
+        if (inComment) {
             this.addToken("CoMF", lexema, lineComment);//comentario nao terminado/ mal formado
         }
 
@@ -270,9 +271,9 @@ public class AnalisadorLexico {
     }
 
     public void addToken(String id, String lexema, int line) {
-            Token t = new Token(id, lexema, line);
-            System.out.println(t);
-            analiseRet+=t+"\n";
-            this.lexema="";
+        Token t = new Token(id, lexema, line);
+        System.out.println(t);
+        analiseRet += t + "\n";
+        this.lexema = "";
     }
 }
